de2e2333dde8ec6f7d2513e933f4a47a486180fa
BrunoCunha1983-creator <brunocunhatlm@gmail.com>
2026-07-29 02:26:02 +0100

    Simplify Pingo Doce config flow for current Home Assistant

--- custom_components/pingo_doce_session/config_flow.py
+++ custom_components/pingo_doce_session/config_flow.py
@@ -5,7 +5,6 @@ from typing import Any
 import voluptuous as vol
 
 from homeassistant import config_entries
-from homeassistant.core import callback
 
 from .const import CONF_SHARED_PATH, DEFAULT_SHARED_PATH, DOMAIN
 
@@ -27,24 +26,3 @@ class PingoDoceSessionConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             {vol.Required(CONF_SHARED_PATH, default=DEFAULT_SHARED_PATH): str}
         )
         return self.async_show_form(step_id="user", data_schema=schema)
-
-    @staticmethod
-    @callback
-    def async_get_options_flow(config_entry):
-        return PingoDoceSessionOptionsFlow(config_entry)
-
-
-class PingoDoceSessionOptionsFlow(config_entries.OptionsFlow):
-    def __init__(self, config_entry) -> None:
-        self.config_entry = config_entry
-
-    async def async_step_init(self, user_input: dict[str, Any] | None = None):
-        if user_input is not None:
-            return self.async_create_entry(title="", data=user_input)
-
-        current = self.config_entry.options.get(
-            CONF_SHARED_PATH,
-            self.config_entry.data.get(CONF_SHARED_PATH, DEFAULT_SHARED_PATH),
-        )
-        schema = vol.Schema({vol.Required(CONF_SHARED_PATH, default=current): str})
-        return self.async_show_form(step_id="init", data_schema=schema)
